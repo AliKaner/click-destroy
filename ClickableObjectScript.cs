11986c70c9a745f7eb94b992442759647acdfb34
Ali Kaner <53481701+AliKaner@users.noreply.github.com>
2021-02-16 22:11:34 +0300

    Add files via upload

---
+++ ClickableObjectScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObjectScript : MonoBehaviour
+{
+    Vector2 newSize;
+    public float x_axisChanger = 3;
+    public float y_axisChanger = 3;
+    public float decreaseRate;
+    public float sizeDeadline;
+    public Sprite spriteDestroy;
+    GameManageScript gameManageScript;
+
+    
+    public void SelfDestroy()
+    {
+        Destroy(gameObject);
+    }
+    public void GettingDestroyed()
+    {
+        Destroy(gameObject);
+        gameManageScript.IncreaseScore();
+
+    }
+    private void Start()
+    {
+        gameManageScript = GameObject.FindWithTag("GameController").GetComponent<GameManageScript>();
+    }
+    private void GettingSmaller()
+    {
+        gameObject.GetComponent<Transform>().localScale = new Vector2(x_axisChanger, y_axisChanger);
+        x_axisChanger -= decreaseRate;
+        y_axisChanger -= decreaseRate;
+
+        if (x_axisChanger < sizeDeadline)
+        {
+            SelfDestroy();
+
+        }
+    }
+    //events...................................................................
+    private void OnMouseDown()
+    {
+        gameObject.GetComponent <SpriteRenderer>().sprite = spriteDestroy;
+        new WaitForEndOfFrame();
+        GettingDestroyed();
+       
+    }
+    
+    //.........................................................................
+    private void Update()
+    {
+        GettingSmaller();
+    }
+}
---
+++ GameManageScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManageScript : MonoBehaviour
+{
+    [SerializeField]
+    int score = 0;
+    [SerializeField]
+    Text scoreText;
+    [SerializeField]
+    Text lwlText;
+
+    public GameObject clone;
+    public float levelMultiplier = 1f;
+    public float spawnTime = 400f;
+    int Tcount = 0;
+    int lwl = 1;
+    public void IncreaseScore()
+    {
+        score += 1;
+        scoreText.text = "Score:" + score;
+        IncreaseLwl();
+    }
+    public void IncreaseLwl()
+    {
+        lwlText.text = "LEVEL " + (lwl+ ((score/10)));
+    }
+    public void ObjDisplay(GameObject game)
+    {
+        Vector2 randomPosition = new Vector2(Random.Range(-9.5f, 9.5f), Random.Range(4f, -4f)); // sonra bak  
+
+        GameObject wordObj = Instantiate(game, randomPosition,transform.rotation);
+    }
+    private void Update()
+    {
+       
+            Tcount++;
+        spawnTime = spawnTime*levelMultiplier;
+        if (Tcount > spawnTime)
+        {
+            ObjDisplay(clone);
+            Tcount = 0;
+        }
+    }
+}
