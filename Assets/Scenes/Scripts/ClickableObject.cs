22014e2fb19456b81507616226122c3419538f86
dkrskl <dogukankrskl@gmail.com>
2021-02-16 16:20:58 +0300

    initial commit

---
+++ Assets/Scenes/Scripts/ClickableObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObject : MonoBehaviour
+{
+    Vector2 newSize;
+    public float reduceAmount = 0.01f;
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
+    }
+
+    private void Update()
+    {
+        if(transform.localScale.x > 0.1)
+        {
+            newSize.x = transform.localScale.x - reduceAmount; 
+            newSize.y = transform.localScale.y - reduceAmount;
+
+            transform.localScale = newSize;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        gameManager.IncreaseScore();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    int score;
+    [SerializeField]
+    Text scoreText;
+
+    public GameObject clickableObjectDummy;
+    int spawnCounter;
+
+    public void IncreaseScore()
+    {
+        score += 1;
+        scoreText.text = "Score: " + score;
+    }
+
+    private void Update()
+    {
+        spawnCounter++;
+
+        if(spawnCounter > 100)
+        {
+            Instantiate(clickableObjectDummy, new Vector2(Random.Range(-6,6), Random.Range(-4,4)), transform.rotation);
+            spawnCounter = 0;
+        }
+    }
+}
